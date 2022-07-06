9ee6631eb8bcab54b229be8d73b9b27582ee50a7
Takayuki Komatsu <komatsutakayuki235@live.com>
2022-07-05 22:11:07 -0300

    add husky

--- jest.config.js
+++ jest.config.js
@@ -6,10 +6,7 @@ module.exports = {
     '@/tests/(.+)': '<rootDir>/tests/$1',
     '@/(.+)': '<rootDir>/src/$1'
   },
-  roots: [
-    '<rootDir>/src',
-    '<rootDir>/tests'
-  ],
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
   transform: {
     '\\.ts$': 'ts-jest'
   }
